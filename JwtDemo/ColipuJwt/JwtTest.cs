7aad4e396f4831b4633e142384fb9258f7e52054
Donnie <donnie.d.yuan@outlook.com>
2018-03-09 14:01:27 +0800

    Framework -v 4.0 jwt

---
+++ JwtDemo/ColipuJwt/JwtTest.cs
@@ -0,0 +1,18 @@
+﻿using JWT;
+using System;
+
+namespace ColipuJwt
+{
+    public class JwtTest
+    {
+        private const string SecretKey = "Colipu-PricingContract-Api";
+        private const string ApplicationId = "PricingContract";
+
+        public static string GenerateJwt()
+        {
+            long tiemStamp = DateTimeHelper.ConvertToTimeStamp(DateTime.Now.AddDays(1));
+
+            return JsonWebToken.Encode(new { applicationId = ApplicationId, issAt = tiemStamp }, SecretKey, JwtHashAlgorithm.HS256);
+        }
+    }
+}
---
+++ JwtDemo/ColipuJwt/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ColipuJwt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string jwt = JwtTest.GenerateJwt();
+
+            Console.WriteLine(jwt);
+
+            Console.ReadKey();
+        }
+    }
+}
