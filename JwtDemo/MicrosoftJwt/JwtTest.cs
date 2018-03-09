ec118562a81b366b8fa318ea2d84f96e1a2b1392
Donnie <donnie.d.yuan@outlook.com>
2018-03-09 14:13:50 +0800

    microsoft package for jwt

---
+++ JwtDemo/MicrosoftJwt/JwtTest.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace MicrosoftJwt
+{
+    public class JwtTest
+    {
+        private const string Secret = "Colipu-Contract-Api";
+
+        public static string GenerateToken(string username, int expireMinutes = 20)
+        {
+            var symmetricKey = Encoding.Default.GetBytes(Secret);
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var now = DateTime.UtcNow;
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name, username)
+                }),
+
+                Expires = now.AddMinutes(Convert.ToInt32(expireMinutes)),
+
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(symmetricKey), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var stoken = tokenHandler.CreateToken(tokenDescriptor);
+            var token = tokenHandler.WriteToken(stoken);
+
+            return token;
+        }
+    }
+}
---
+++ JwtDemo/MicrosoftJwt/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MicrosoftJwt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string jwt = JwtTest.GenerateToken("Donnie");
+
+            Console.WriteLine(jwt);
+
+            Console.ReadKey();
+        }
+    }
+}
